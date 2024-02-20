5241e1db116c3a37b61f6fb680614521b2300b76
SRobles97 <s.robles01@ufromail.cl>
2024-02-20 13:14:02 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:games_app/screens/home.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const GamesApp());
+}
+
+class GamesApp extends StatelessWidget {
+  const GamesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: MaterialApp(
+            title: 'Videogames Flutter App',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const HomeScreen()));
+  }
+}
